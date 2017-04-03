3629e56d80ecaca971828d1ecbbddcd7baa1dd7e
Jason Bock <jason@jasonbock.net>
2017-04-03 17:42:30 -0500

    Added class for code snippet demo

---
+++ Csla.AutoAddBusinessRules/Hello.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Csla.AutoAddBusinessRules
+{
+	public sealed class Hello
+	{
+		public Hello(string name)
+		{
+			if(name == null)
+			{
+				throw new ArgumentNullException(nameof(name));
+			}
+
+			this.Name = name;
+		}
+
+		public string Speak()
+		{
+			return $"Hello {this.Name}";
+		}
+
+		public string Name { get; }
+	}
+}
